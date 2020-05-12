e9bbb00a40d72e2f1402c1692f6fe55574c9dcc1
pikktorr <viktor.rozgonyi@gmail.com>
2020-05-12 18:38:29 +0200

    CRP javascript

--- 03_crit_render_path/script.js
+++ 03_crit_render_path/script.js
@@ -1,7 +1,10 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  console.log("DOM fully loaded and parsed", event);
-});
-window.addEventListener("load", (event) => {
-  console.log("All resources finished loading", event);
-});
+// 1. READING THE JAVASCRIPT FILE
 console.log("this is the first script");
+// 2. FINISH THE BUILT DOM TREE
+document.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM fully loaded and parsed");
+});
+// 3. FINISH LOADING ALL THE FILES
+window.addEventListener("load", (event) => {
+  console.log("All resources finished loading");
+});
